fc87eeb87b4c5f57ed1a54f14cd1a0af66edaab8
Simon Hengel <sol@typeful.net>
2025-02-22 06:00:27 +0700

    Correctly handle streaming request bodies

--- test/VCRSpec.hs
+++ test/VCRSpec.hs
@@ -6,16 +6,23 @@ import Imports
 
 import Test.Hspec
 import Test.Mockery.Directory
-
 import Data.ByteString.Lazy qualified as L
 import Network.HTTP.Client qualified as Client
 import Network.HTTP.Client.TLS (getGlobalManager)
 import Network.HTTP.Types
 import System.Directory
 
+import Data.ByteString (ByteString)
+import Network.HTTP.Client.Internal qualified as Client
+
+
 import WebMock
 
 import VCR
+
+import           Test.HUnit.Lang
+
+import           Data.Aeson
 
 makeRequest :: String -> [Header] -> IO (Client.Response L.ByteString)
 makeRequest url headers = do
@@ -26,11 +33,54 @@ makeRequest url headers = do
 httpException :: Client.HttpException -> Bool
 httpException _ = True
 
+hUnitFailure :: FailureReason -> HUnitFailure -> Bool
+hUnitFailure expectd (HUnitFailure _ actual) = expectd == actual
+
+expectedButGot :: Request -> Request -> HUnitFailure -> Bool
+expectedButGot expected actual= hUnitFailure (ExpectedButGot Nothing (show expected) (show actual))
+
+infix 1 `shouldReturnBody`
+infix 1 `shouldReturnStatus`
+
+shouldReturnStatus :: HasCallStack => String -> Status -> IO ()
+shouldReturnStatus url expected = do
+  Client.responseStatus <$> makeRequest url [] `shouldReturn` expected
+
+shouldReturnBody :: HasCallStack => String -> L.ByteString -> IO ()
+shouldReturnBody url expected = do
+  Client.responseBody <$> makeRequest url [] `shouldReturn` expected
+
 tape :: Tape
 tape = "tape.yaml"
 
+data HTTPBin = HTTPBin {
+  data_ :: String
+} deriving (Eq, Show)
+
+instance FromJSON HTTPBin where
+  parseJSON = withObject "HTTPBin" \ o -> HTTPBin <$> o .: "data"
+
 spec :: Spec
 spec = around_ inTempDirectory $ do
+  describe "withTape" do
+    it "xxx requestBody" do
+      withTape tape do
+        manager <- getGlobalManager
+        request <- Client.parseUrlThrow "http://httpbin.org/anything"
+
+        _body :: IO ByteString <- Client.constBodyReader ["xxx Foo xxx\n"]
+
+        let
+          body = Client.RequestBodyStreamChunked bar
+
+          bar :: (IO ByteString -> IO ()) -> IO ()
+          bar action = action _body
+
+        response <- Client.httpLbs request { Client.method = "POST", Client.requestBody = body } manager
+
+        r :: HTTPBin <- throwDecode $ Client.responseBody response
+        r.data_ `shouldBe` "xxx Foo xxx\n"
+
   describe "withTape" $ do
     context "with an Authorization header" $ do
       let
